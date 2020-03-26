5e2325191b02f72fc28d897c8c0bc6ca888c10b9
prosoftwaredevelopment <softwaredeveloper1234567890@gmail.com>
2020-03-26 19:35:10 +0000

    calculator app added

--- djangoprojects/calculatorapp/views.py
+++ djangoprojects/calculatorapp/views.py
@@ -5,3 +5,25 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    try:
+        ans = eval(q)
+        mydictionary = {
+            "q" : q,
+            "ans" : ans,
+            "error" : False,
+            "result" : True
+        }
+        return render(request,'index.html',context=mydictionary)
+    except:
+        mydictionary = {
+            "error" : True,
+            "result" : False
+
+        }
+        return render(request,'index.html',context=mydictionary)
+
+
+        
