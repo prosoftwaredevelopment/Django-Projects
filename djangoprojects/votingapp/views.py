e58810a7f1e775ff6429c8dc0dff5ad9e07083c7
prosoftwaredevelopment <softwaredeveloper1234567890@gmail.com>
2020-04-12 15:54:36 +0100

    lect 24

--- djangoprojects/votingapp/views.py
+++ djangoprojects/votingapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse,HttpResponse
 # Create your views here.
 def index(request):
     arr = ['Java', 'Python', 'Cplusplus', 'C', 'DotNET', 'JavaScript', 'PHP', 'Swift', 'SQL', 'Ruby', 'Delphi', 'Objective-C', 'Go', 'Assemblylanguage', 'VisualBasic', 'D', 'R', 'Perl', 'MATLAB']
@@ -7,3 +8,6 @@ def index(request):
     }
     return render(request,'index.html',context=mydictionary)
 
+def getquery(request):
+    q = request.GET['languages']
+    return HttpResponse(q)
